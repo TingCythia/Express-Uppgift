105a8621cdfcb39015619a066438b4cf71c1b718
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-09-15 17:22:41 +0200

    finished by teacher check

--- client/client.js
+++ client/client.js
@@ -1,46 +1,31 @@
 
- var obj1;
-
- fetch('http://localhost:5003/userRouter/externalApi')
-  .then(res => res.json())
-  .then(data => obj1 =data)
-  .then(() => console.log(obj1))
- 
-
 
   const getAllUsers = async (params) => { 
     try {
-        var array =[obj1]
         
         let originObj = document.getElementById("showAllUsers")
         originObj.innerHTML=""
-        await fetch("http://localhost:5003/userRouter")
-        .then((response) => {
-            return response.json()
-        }).then((body) => {
-        let array3 = array.concat(body)
-          array3.map((b) => {
+        let response = await fetch("http://localhost:5003/userRouter")
+        let userlist = await response.json()
+        
+        userlist.map((b) => {
             const ul = document.createElement("ul");
             const li = document.createElement("li");
             li.textContent ="ID: "+ b.id
+            const li2 = document.createElement("li");
+            li2.textContent ="Gender: "+ b.gender
             const li3 = document.createElement("li");
-            li3.textContent ="User Title: "+ b.title
+            li3.textContent ="First Name: "+ b.firstName
             const li4 = document.createElement("li");
-            li4.textContent ="User description: "+ b.body
-            ul.append(li,li3,li4)
-            originObj.append(ul)         
-        })
-        }).catch((err) => {
-            throw err
-        })
+            li4.textContent ="Last Name: "+ b.lastName
+            ul.append(li,li2,li3,li4)
+            originObj.append(ul)     
+        })    
+        
     } catch(err) {
         console.error(err)
     }
  }  
-
-
- document.getElementById("getAllBtn").addEventListener("click", getAllUsers
-)
 
 
 const getById = async (event) => {
@@ -53,9 +38,16 @@ const getById = async (event) => {
         })
         .then((body) =>{
         console.log(body)
+        if(typeof body == "string"){
+            showGetIdValue.innerHTML = body
+            return
+        }
         showGetIdValue.innerHTML=`
         <ul>
         <li>ID is : ${body.id}</li>
+        <li>FirstName is : ${body.firstName}</li>
+        <li>LastName is : ${body.lastName}</li>
+        <li>Gender is : ${body.gender}</li>
         </ul>
         `
         })
@@ -65,10 +57,29 @@ const getById = async (event) => {
     } 
 
 
+const getRandomUser = (e) =>{
+    fetch("http://localhost:5003/userRouter/externalApi", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+    })
+    .then(res => res.json())
+    .then(body => {
+     console.log(body)
+     showNewUser.innerHTML=body
+    })
+    .catch(err => console.log(err));
+}
+
+document.getElementById("randomUser").addEventListener("click", getRandomUser)
+
 const addNewUsers = async (event) => {
 
-    let inputTitle = document.getElementById("title").value;
-    let inputBody = document.getElementById("body").value;
+    let Firstname = document.getElementById("firstname").value;
+    let lastName = document.getElementById("lastname").value;
+    let gender = document.getElementById("gender").value;
     let showNewUser= document.getElementById("showNewUser")
     fetch("http://localhost:5003/userRouter", {
         method: "POST",
@@ -77,16 +88,15 @@ const addNewUsers = async (event) => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            title: inputTitle,
-            body:inputBody
+            first_name: Firstname,
+            last_name: lastName,
+            gender:gender
         })
     })
     .then(res => res.json())
     .then(data => {
      console.log(data)
-     showNewUser.innerHTML=`
-     user has added success!
-     `
+     showNewUser.innerHTML=data
     })
     .catch(err => console.log(err));
 
@@ -94,9 +104,11 @@ const addNewUsers = async (event) => {
 
 const updateUsers = async (event) => {
     let findID = document.getElementById("findID").value;
-    let updateTitle = document.getElementById("updateTitle").value;
+    const Firstname= document.getElementById("updateFirstName").value;
+    const lastName = document.getElementById("lastname").value;
+    const gender = document.getElementById("gender").value;
     let showUpdateValue= document.getElementById("showUpdateValue")
-    fetch("http://localhost:5003/userRouter", {
+    fetch(`http://localhost:5003/userRouter/${findID}`, {
         method: "PUT",
         headers: {
             Accept: "application/json",
@@ -104,7 +116,9 @@ const updateUsers = async (event) => {
         },
         body: JSON.stringify({ 
             id:findID, 
-            title: updateTitle,
+            firstName: Firstname,
+            lastName:lastName,
+            gender: gender
         })
     })
     .then(res => res.json())
@@ -144,7 +158,7 @@ const deleteByUserId= async (event) => {
 
     } 
 
-
+document.getElementById("getAllBtn").addEventListener("click", getAllUsers)
 document.getElementById("GetById").addEventListener("click", getById) 
 document.getElementById("addNewUser").addEventListener("click", addNewUsers)
 document.getElementById("update").addEventListener("click", updateUsers)
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,131 +1,143 @@
 import express from "express";
 import fetch from "node-fetch";
-import { nanoid } from "nanoid"  
+import { nanoid } from "nanoid"
 export const router = express.Router();
 
 
-let userList =[] ;
+let userList = [];
 let user;
-/* Get external api */
-router.get(`/externalApi`, async function (req, res) {
-    try {   
-        const url =
-        'https://jsonplaceholder.typicode.com/posts/1';
-        let response =  await fetch(url);
-        let body = await response.json();
-        res.json(body)
-    } catch (err) {
-        console.log(err);
-        res.status(404).json(err.message)
-    } 
-});
+let randomUser;
+
 
 /* Get all request body */
 router.get(`/`, async function (req, res) {
-        try {   
-            res.json(userList)
-        } catch (err) {
-            console.log(err);
-            res.status(404).json(err.message)
-        } 
-    });
+    try {
+        res.json(userList)
+    } catch (err) {
+        console.log(err);
+        res.status(404).json(err.message)
+    }
+});
+/* Get external api */
+router.post(`/externalApi`, async function (req, res) {
+    try {
+        const url =
+            'https://random-data-api.com/api/v2/users?size=2&is_xml=true';
+        let response = await fetch(url);
+        let body = await response.json();
+        randomUser = {
+            id: body[0].id,
+            firstName: body[0].first_name,
+            lastName: body[0].last_name,
+            gender: body[0].gender
+        }
+        userList.push(randomUser)
+        console.log(randomUser)
+        res.json("Random user from Api is saved")
+    } catch (err) {
+        console.log(err);
+        res.status(404).json(err.message)
+    }
+});
 
 /* post new user  */
- router.post("/", async (req, res)=>{
-    try{
-        const userExists = userList.find(users => users.title == req.body.title)
-    if (userExists){
+router.post("/", async (req, res) => {
+    try {
+        const userExists = userList.find(users => users.firstName == req.body.first_name)
+        if (userExists) {
             throw new Error("User already exists")
         }
-         user = {
+        user = {
             id: nanoid(),
-            title: req.body.title,
-            body:req.body.body
-           }
+            firstName: req.body.first_name,
+            lastName: req.body.last_name,
+            gender: req.body.gender
+        }
         userList.push(user)
-        return res.status(201).json(user)
-        
-    }catch(err){
+        return res.status(201).json("New user add success")
+
+    } catch (err) {
         res.status(400).json(err.message)
     }
 
-})  
+})
 
-    /* Get all specific ID */
-    router.get("/:id",  async (req, res) =>{
+/* Get all specific ID */
+router.get("/:id", async (req, res) => {
 
-            // promise syntax
-                try {
+    // promise syntax
+    try {
 
-                    const foundUser = userList.find((user)=>{
-                       if(user.id == req.params.id) {
-                           console.log(user.id)
-                           return true
-                       }
-                    })
-                    if(!foundUser) {
-                        throw new Error("Id does not exists..")
-                     }
-                     res.json(foundUser)
-                } catch (err) {
-                    console.log(err);
-                    res.status(404).json(err.message)
-                };
-        
-    })
+        const foundUser = userList.find((user) => {
+            if (user.id == req.params.id) {
+                console.log(user.id)
+                return true
+            }
+        })
+        if (!foundUser) {
+            throw new Error("Id does not exists..")
+        }
+        res.json(foundUser)
+    } catch (err) {
+        console.log(err);
+        res.status(404).json(err.message)
+    };
+
+})
 
 
 /* Put value */
-router.put("/", async (req, res) => {
+router.put("/:id", async (req, res) => {
 
     try {
-        const indexToUpdate = userList.findIndex(user =>
-          user.id == req.body.id)
-           
-        if(indexToUpdate == -1) {
-            throw new Error("Can not find update value")
-         }
-         
-         const titleBeforeUpdate = userList[indexToUpdate].title
 
-         userList[indexToUpdate] = req.body
-         
-         console.log(req.body.title)
-         res.json(`User with old name ${titleBeforeUpdate} has been updated to  "${userList[indexToUpdate].title}"!`)
+        const indexToUpdate = userList.findIndex(user =>
+            user.id == req.params.id)
+        if (indexToUpdate == -1) {
+            throw new Error("Can not find update value")
+        }
+
+
+        const titleBeforeUpdate = userList[indexToUpdate].firstName
+
+        userList[indexToUpdate] = req.body
+
+        console.log(req.body)
+        res.json(`User with old name ${titleBeforeUpdate} has been updated to  "${userList[indexToUpdate].firstName}"!`)
 
     } catch (err) {
         console.log(err);
         res.status(404).json(err.message)
     };
-}) 
+})
 
 /* Delete value */
-router.delete("/:userId", async (req, res) => {
+router.delete("/:id", async (req, res) => {
 
     try {
         const indexToRemove = userList.findIndex(user =>
-          user.id == req.params.userId)
-           
-        if(indexToRemove == -1) {
-            throw new Error("Can not find delete value")
-         }
-         
-         const titleToRemove = userList[indexToRemove].title
+            user.id == req.params.id)
 
-         userList.splice(indexToRemove, 1)
-         
-         console.log("Remove success")
-         res.json(`Users with title "${titleToRemove} "is removed!`)
+        if (indexToRemove == -1) {
+            throw new Error("Can not find delete value")
+        }
+
+        const titleToRemove = userList[indexToRemove].firstName
+
+        userList.splice(indexToRemove, 1)
+
+        console.log("Remove success")
+        res.json(`Users with title "${titleToRemove} "is removed!`)
 
     } catch (err) {
         console.log(err);
         res.status(404).json(err.message)
     };
-}) 
+})
 
 
 
-router.use((err, req, res, next) =>{
+router.use((err, req, res, next) => {
     console.log(err.status)
     console.log(err.message)
     res.status(500).json(err)
